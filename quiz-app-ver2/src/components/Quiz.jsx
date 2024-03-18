30985f9af38797ceab0fd6de900d362b8fcbc785
min young <minypodo@gmail.com>
2024-03-18 23:45:45 +0900

    - 답 랜덤하게 나오기 - 퀴즈 완료 페이지

--- quiz-app-ver2/src/components/Quiz.jsx
+++ quiz-app-ver2/src/components/Quiz.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react"
 import QUESTION from "../question.js"
+import quizEndImg from "../assets/quiz-complete.png"
 
 export default function Quiz() {
     const [userAnswers, setUserAnswers] = useState([])
 
     const activeQuestionIndex = userAnswers.length
+
+    const isQuizCompleted = activeQuestionIndex === QUESTION.length
 
     function activeAnswersHandler(selectedAnswer) {
         setUserAnswers(prevAnswers => {
@@ -12,13 +15,25 @@ export default function Quiz() {
         })
     }
 
+    if (isQuizCompleted) {
+        return (
+            <div id="summary">
+                <img src={quizEndImg} alt="quiz end!" />
+                <h2>퀴즈가 완료되었습니다!</h2>
+            </div>
+        )
+    }
+
+    const shuffledAnswers = [...QUESTION[activeQuestionIndex].answers]
+    shuffledAnswers.sort(() => Math.random() - 0.5)
+
     return (
         <div id="quiz">
             <div id="question">
                 <h2>{QUESTION[activeQuestionIndex].text}</h2>
             </div>
             <ul id="answers">
-                {QUESTION[activeQuestionIndex].answers.map(answer => {
+                {shuffledAnswers.map(answer => {
                     return (
                         <li key={answer} className="answer">
                             <button onClick={() => activeAnswersHandler(answer)}>{answer}</button>
