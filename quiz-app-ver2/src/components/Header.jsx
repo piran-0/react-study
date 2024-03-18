c5c88c896c7981934335b1710fff586327b3ede9
min young <minypodo@gmail.com>
2024-03-18 22:46:06 +0900

    quiz 컴포 기초로직 - 배열로 구성되어 있는 js 파일을 가져와서 화면에 표시하기 - 버튼 클릭시 다음 문제로 넘어가는 로직

--- quiz-app-ver2/src/components/Header.jsx
+++ quiz-app-ver2/src/components/Header.jsx
@@ -2,7 +2,7 @@ import logo from "../assets/quiz-logo.png"
 
 export default function Header() {
     return <header>
-        <img src={logo} alt="" />
+        <img src={logo} alt="리액트 퀴즈 앱의 로고" />
         <h1>!리액트 퀴즈!</h1>
     </header>
 }
--- quiz-app-ver2/src/components/Quiz.jsx
+++ quiz-app-ver2/src/components/Quiz.jsx
@@ -1,5 +1,31 @@
-export default function Quiz() {
-    return <div id="quiz">
+import { useState } from "react"
+import QUESTION from "../question.js"
 
-    </div>
+export default function Quiz() {
+    const [userAnswers, setUserAnswers] = useState([])
+
+    const activeQuestionIndex = userAnswers.length
+
+    function activeAnswersHandler(selectedAnswer) {
+        setUserAnswers(prevAnswers => {
+            return [...prevAnswers, selectedAnswer]
+        })
+    }
+
+    return (
+        <div id="quiz">
+            <div id="question">
+                <h2>{QUESTION[activeQuestionIndex].text}</h2>
+            </div>
+            <ul id="answers">
+                {QUESTION[activeQuestionIndex].answers.map(answer => {
+                    return (
+                        <li key={answer} className="answer">
+                            <button onClick={() => activeAnswersHandler(answer)}>{answer}</button>
+                        </li>
+                    )
+                })}
+            </ul>
+        </div>
+    )
 }
