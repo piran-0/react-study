b207005e75042fe3992d7c1de28204a75d0186e1
min young <minypodo@gmail.com>
2024-03-22 18:41:32 +0900

    http 요청

--- api-react/src/components/AvailablePlaces.jsx
+++ api-react/src/components/AvailablePlaces.jsx
@@ -1,12 +1,26 @@
+import { useState, useEffect } from 'react';
 import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([])
+
+  useEffect(() => {
+    async function fetchPlaces() {
+      const res = await fetch("http://localhost:3000/places")
+      const resData = await res.json()
+      setAvailablePlaces(resData.places)
+    }
+    fetchPlaces()
+  }, []
+  )
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
   );
 }
+
