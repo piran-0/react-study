5ed207d47c524da364d136ba982f698ad48bd055
min young <minypodo@gmail.com>
2024-03-25 17:38:25 +0900

    put 요청

--- api-react/src/http.js
+++ api-react/src/http.js
@@ -8,3 +8,21 @@ export async function fetchAvailablePlaces() {
 
   return resData.places;
 }
+
+export async function updateUserPlaces(places) {
+  const res = await fetch("http://localhost:3000/user-places", {
+    method: "PUT",
+    body: JSON.stringify({ places }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const resData = await res.json();
+
+  if (!res.ok) {
+    throw new Error("Fail to update user data");
+  }
+
+  return resData.message;
+}
