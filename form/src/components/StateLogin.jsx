1b5e2c72a5abdadd68b79882717bd91a79e96463
min young <minypodo@gmail.com>
2024-03-30 18:12:37 +0900

    커스텀 리액트 훅으로 코드 줄이기

--- form/src/components/StateLogin.jsx
+++ form/src/components/StateLogin.jsx
@@ -1,52 +1,21 @@
-import { useState } from "react";
 import Input from "./Input";
 import { isEmail, isNotEmpty, hasMinLength } from "../util/validation.js"
+import { useInput } from "../hooks/useInput.js";
 
 export default function Login() {
-  // const [enteredEmail, setEnteredEmail] = useState("")
-  // const [enteredPassword, setEnteredPassword] = useState("")
-  const [enteredValues, setEnterValues] = useState({
-    email: "",
-    password: "",
-  });
+  const { value: emailValue, handleInputChange: handleEmailChange, handleInputBlur: handleEmailBlur, hasError: emailHasError }
+    = useInput("", (value) => isEmail(value) && isNotEmpty(value))
 
-  const [didEdit, setDidEdit] = useState({
-    email: false,
-    password: false,
-  });
-
-  const emailIsInvalid = didEdit.email && !isEmail(enteredValues.email) && !isNotEmpty(enteredValues.email)
-  const pwdIsInvalid = didEdit.password && !hasMinLength(enteredValues.password, 6)
+  const { value: pwdValue, handleInputChange: handlePwdChange, handleInputBlur: handlePwdBlur, hasError: pwdHasError }
+    = useInput("", value => hasMinLength(value, 6))
 
   function handleSubmit(event) {
-    //HTTP 요청X
     event.preventDefault();
-    console.log(
-      "User email: " + enteredEmail + " User Password: " + enteredPassword
-    );
 
-    setEnterValues({
-      email: "",
-      password: "",
-    });
-  }
-
-  function handleInputChange(identifier, value) {
-    setEnterValues((prevValues) => ({
-      ...prevValues,
-      [identifier]: value,
-    }));
-    setDidEdit((prevEdit) => ({
-      ...prevEdit,
-      [identifier]: false,
-    }));
-  }
-
-  function handleInputBlur(identifier) {
-    setDidEdit((prevEdit) => ({
-      ...prevEdit,
-      [identifier]: true,
-    }));
+    if (emailHasError || pwdHasError) {
+      return
+    }
+    console.log(emailValue, pwdValue)
   }
 
   return (
@@ -59,10 +28,10 @@ export default function Login() {
           id="email"
           type="email"
           name="email"
-          onChange={(e) => handleInputChange("email", e.target.value)}
-          value={enteredValues.email}
-          onBlur={() => handleInputBlur("email")}
-          error={emailIsInvalid && "유효한 이메일을 적어주세요"}
+          onChange={handleEmailChange}
+          value={emailValue}
+          onBlur={handleEmailBlur}
+          error={emailHasError && "유효한 이메일을 적어주세요"}
         />
 
         <Input
@@ -70,10 +39,10 @@ export default function Login() {
           id="password"
           type="password"
           name="password"
-          onChange={(e) => handleInputChange("password", e.target.value)}
-          value={enteredValues.password}
-          onBlur={() => handleInputBlur("password")}
-          error={pwdIsInvalid && "유효한 비밀번호를 적어주세요"}
+          onChange={handlePwdChange}
+          value={pwdValue}
+          onBlur={handlePwdBlur}
+          error={pwdHasError && "유효한 비밀번호를 적어주세요"}
         />
 
       </div>
--- form/src/hooks/useInput.js
+++ form/src/hooks/useInput.js
@@ -1,1 +1,24 @@
-export function useInput() {}
+import { useState } from "react";
+
+export function useInput(defaultValue, validationFn) {
+  const [enteredValue, setEnterValue] = useState(defaultValue);
+  const [didEdit, setDidEdit] = useState(false);
+
+  const valueIsValid = validationFn(enteredValue);
+
+  function handleInputChange(e) {
+    setEnterValue(e.target.value);
+    setDidEdit(false);
+  }
+
+  function handleInputBlur(identifier) {
+    setDidEdit(true);
+  }
+
+  return {
+    value: enteredValue,
+    handleInputBlur,
+    handleInputChange,
+    hasError: didEdit && !valueIsValid,
+  };
+}
